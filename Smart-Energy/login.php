d6e033a4fb4db4c43f95e92f31350d1dbc92a991
Mohammed6035258 <mohammednl2005@gmail.com>
2025-06-19 14:26:15 +0200

    mappen bij elkaar

--- Smart-Energy/login.php
+++ Smart-Energy/login.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html>
-<head><link rel="stylesheet" href="login.css"></head>
+<head><link rel="stylesheet" href="css/login.css"></head>
 <body>
 <div class="login-container">
     <div class="login-header">Login</div>
