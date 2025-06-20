0d3e3a11dcfa96ac698a835741e631c27d26f569
samneuteboom <6040120@mborijnland.nl>
2025-06-20 10:43:09 +0200

    dashboard toegevoegd

---
+++ Smart-Energy/dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head><link rel="stylesheet" href="css/home.css"></head>
+<body>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="logout.php">Logout</a>
+</nav>
+<h1>Welcome, <?= $_SESSION['username'] ?>!</h1>
+</body>
+</html>
--- Smart-Energy/login.php
+++ Smart-Energy/login.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['username'] = $username;
-        header('Location: index.php');
+        header('Location: dashboard.php');
     } else {
         $message = 'Incorrect credentials';
     }
