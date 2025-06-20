4713cb32f9297814d2b92d1f1eb2717e8fd21189
samneuteboom <6040120@mborijnland.nl>
2025-06-20 11:00:29 +0200

    dashboard werkend maken met een sesion start

--- Smart-Energy/dashboard.php
+++ Smart-Energy/dashboard.php
@@ -1,9 +1,13 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head><link rel="stylesheet" href="css/home.css"></head>
 <body>
 <nav>
     <a href="index.php">Home</a>
+    <a href=""></a>
     <a href="logout.php">Logout</a>
 </nav>
 <h1>Welcome, <?= $_SESSION['username'] ?>!</h1>
